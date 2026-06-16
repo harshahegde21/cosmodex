919d078edb1c3d70fa18b7fcd444ceb5bbbfb3ab
priyanshu <priyanshuofficial2004@gmail.com>
2026-06-17 00:11:21 +0530

    refactor: merge dashboard css into globals.css

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./dashboard.css";
 
 const ayahaFont = localFont({
   src: "../../../fonts/ayaha-2-font/AyahaRegularDemowthswshes-7OLKK.ttf",
